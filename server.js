b59c99fa5c7459267ece4e6a98f2d9df8bc81a6a
anumi1999 <52823306+anumi1999@users.noreply.github.com>
2019-10-17 21:43:07 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,27 +3,46 @@ var express = require('express');
 var session = require('express-session');
 var bodyParser = require('body-parser');
 var path = require('path');
+var fs = require('fs');
+var http= require('http');
+var url = require('url')
 var connection = mysql.createConnection({
 	host     : 'localhost',
 	user     : 'root',
-	password : 'codybanks',
+	password : '',
 	database : 'nodelogin'
 });
-
 var app = express();
 app.use(session({
 	secret: 'secret',
 	resave: true,
 	saveUninitialized: true
 }));
+http.createServer(function(req,res){
+      fs.readFile("./views/hddx1.html","UTF-8",function(err,data){
+			  res.writeHead(200, {"Content-Type" : "text\html"});
+			  res.end(html);
+
+	  });
+	  console.log(req.url);
+});
 app.use(bodyParser.urlencoded({extended : true}));
 app.use(bodyParser.json());
-
 app.get('/', function(request, response) {
-	response.sendFile(path.join(__dirname + '/login.html'));
+	      
+            response.sendFile(path.join(__dirname + '/views/hddx1.html'));
+            
+});
+app.post('/log', function(request, response) {
+	
+			response.redirect('/h');		
+			response.end();
+		});
+app.get('/h', function(request, response) {
+	response.sendFile(path.join(__dirname + '/views/login.html'));
 });
 
-app.post('/auth', function(request, response) {
+    app.post('/auth', function(request, response) {
 	var username = request.body.username;
 	var password = request.body.password;
 	if (username && password) {
@@ -34,7 +53,7 @@ app.post('/auth', function(request, response) {
 				response.redirect('/home');
 			} else {
 				response.send('Incorrect Username and/or Password!');
-			}
+			}			
 			response.end();
 		});
 	} else {
@@ -50,5 +69,5 @@ app.get('/home', function(request, response) {
 		response.send('Please login to view this page!');
 	}
 	response.end();
-});
+})
 app.listen(8080);
